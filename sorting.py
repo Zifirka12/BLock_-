89c0957cd03bb661c9c7d4491bad4098730aa627
Zifirka <Ilaafanasef21@gmail.com>
2024-09-23 15:05:44 +0300

    15.1

--- sorting.py
+++ sorting.py
@@ -23,6 +23,12 @@ class Product:
     def new_product(cls, product_info: dict) -> "Product":
         return cls(product_info["name"], product_info["description"], product_info["price"], product_info["quantity"])
 
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return self.price * self.quantity + other.price * other.quantity
+
 
 class Category:
     category_count = 0
@@ -39,7 +45,41 @@ class Category:
         self._products.append(product)
         Category.product_count += 1
 
+    def get_products(self) -> list:
+        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self._products]
+
     @property
-    def products(self) -> str:
-        return "\n".join([f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
-                          for product in self._products])
+    def products(self) -> list:
+        return self.get_products()
+
+    def __str__(self):
+        total_items = sum(product.quantity for product in self._products)
+        return f"{self.name}, количество продуктов: {total_items} шт."
+
+
+class ProductList:
+    def __init__(self, *args):
+        self.products = args
+
+    def add_product(self, name, price):
+        self.products = (*self.products, (name, price))
+
+    def get_total_cost(self):
+        return sum(price * quantity for _, price, quantity in self.products)
+
+    def display_summary(self):
+        items = [(name, price) for name, price, _ in self.products]
+        return ", ".join(f"{name}: {price}" for name, price in items)
+
+    def __add__(self, other):
+        combined_list = ProductList(*self.products, *other.products)
+        return combined_list
+
+    def __repr__(self):
+        return f"Products({self.display_summary()})"
+
+    def __len__(self):
+        return len(self.products)
+
+    def __iter__(self):
+        return iter(self.products)
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -24,6 +24,22 @@ def test_category_creation() -> None:
     assert len(category.products) == 0
 
 
+def test_sum_products() -> None:
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    assert product2 + product1 == 2580000.0
+
+
+def test_str_category() -> None:
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет", 180000.0, 5)
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1],
+    )
+    assert str(category1) == "Смартфоны, количество продуктов: 5 шт."
+
+
 if __name__ == "__main__":
     test_product_creation()
     test_price_setter_getter()
